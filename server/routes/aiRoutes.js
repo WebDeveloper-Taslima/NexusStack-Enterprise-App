c1e72061a27ef392103e18a38035ceee7ad19585
Taslima Akter <sharlyaman0987@gmail.com>
2026-08-12 01:11:08 +0600

    Delete server directory

--- server/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { aiController } from '../controllers/aiController.js';
-
-const router = Router();
-
-router.get('/prompts', aiController.getPromptTemplates);
-router.post('/stream', aiController.streamPrompt);
-
-export default router;
